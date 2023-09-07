41d413e3f7efc07b3f15252db86458295a9a5e15
SeanG-rsd <70712778+SeanG-rsd@users.noreply.github.com>
2023-09-06 17:24:20 -0700

    UI and bullet fix

--- Battle Tanks/Assets/Scripts/GamePlay/Bullet.cs
+++ Battle Tanks/Assets/Scripts/GamePlay/Bullet.cs
@@ -15,6 +15,9 @@ public class Bullet : MonoBehaviour
     public int damage = 0;
 
     public Player parentPlayer;
+
+    private float noDieTime = 0.2f;
+    private bool noDie = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +27,14 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (noDie)
+        { 
+            noDieTime -= Time.deltaTime;
+            if (noDieTime < 0)
+            {
+                noDie = false;
+            }
+        }
     }
 
     public void Shoot(float speed, float size, Transform origin, int dam)
@@ -33,5 +43,20 @@ public class Bullet : MonoBehaviour
         transform.localScale = size * transform.localScale;
         rb.AddForce(origin.forward * speed);
         damage = dam;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!noDie)
+        {
+            GetComponent<PhotonView>().RPC("OnDestroy", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+
+    private void OnDestroy()
+    {
+        Destroy(gameObject);
     }
 }
